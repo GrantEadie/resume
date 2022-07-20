5864c1146b71292371bceb8b8eee872fb7bc28f9
Grant Eadie <grantleadie@gmail.com>
2022-07-20 06:52:48 -0700

    add the hide menu ability.

--- src/comps/Header.js
+++ src/comps/Header.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Squash as Hamburger } from "hamburger-react";
 import { slide as Menu } from "react-burger-menu";
 import { Link } from "react-router-dom";
@@ -7,10 +7,31 @@ import { House, Bag, AddressBook } from "phosphor-react";
 
 const Header = () => {
   const [isOpen, setOpen] = useState(false);
+  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [visible, setVisible] = useState(true);
+
+  const handleScroll = useCallback(() => {
+    const currentScrollPos = window.pageYOffset;
+    setVisible(
+      (prevScrollPos > currentScrollPos) ||
+        currentScrollPos < 10
+    );
+
+    setPrevScrollPos(currentScrollPos);
+  }, [prevScrollPos]);
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [prevScrollPos, visible, handleScroll]);
+
   return (
     <div id="header">
       <div id="header-logo">g.e.</div>
-      <div id="burger">
+      <div
+        id="burger"
+        style={{ top: visible ? "0" : "-100px", transition: "top .2s ease" }}
+      >
         <Hamburger
           color={!isOpen ? "var(--red)" : "var(--blue)"}
           toggled={isOpen}
